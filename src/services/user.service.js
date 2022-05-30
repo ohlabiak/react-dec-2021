8df06dcfd05921b5a4b7197236cda3324cded6af
Vasil.Ohlabiak <vasiliu.ohlabiak@gmail.com>
2022-05-30 15:56:01 +0300

    commit

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,7 +2,7 @@ import {axiosService} from "./axios.service";
 import {urls} from "../constants";
 
 const userService = {
-    getAll:()=>axiosService.get(urls.users)
+    getAll: ()=>axiosService.get(urls.users)
 }
 
 export {

53096b81780e377e6223eb583b92a2ced0e4fe84
Vasil.Ohlabiak <vasiliu.ohlabiak@gmail.com>
2022-06-06 23:42:33 +0300

    commit

---
+++ src/components/User/User.js
@@ -0,0 +1,12 @@
+import {Link} from "react-router-dom";
+
+const User = ({user}) => {
+    const [id, name] = user;
+    return (
+        <div>
+            {id}--{name} <Link to={`${id}`} state={user}>get Detail</Link>
+        </div>
+    );
+};
+
+export {User};
--- src/pages/UsersPage/UsersPage.js
+++ src/pages/UsersPage/UsersPage.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
+import {userService} from "../../services";
+import {User} from "../../components";
+import {Outlet} from "react-router-dom";
 
 const UsersPage = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        userService.getAll().then(({data}) => setUsers(data))
+        },[])
+
     return (
         <div>
-users
+            <div>{users.map(user => <User key={user.id} user={user}/>)}</div>
+            <div><Outlet/></div>
         </div>
     );
 };
